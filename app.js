79e428522275a4451bf824551e20b49880995099
melesejr <74154647+melesejr@users.noreply.github.com>
2021-08-20 01:00:46 +0300

    add API url key for unsplash random fhotos

---
+++ app.js
@@ -0,0 +1,4 @@
+// Unsplash Api
+const count = 10;
+const apiKey = 'bv5VfP7Ab13j7yXXS9PBC_HzLvIRZgUFhUs_VeMYikA'; 
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
