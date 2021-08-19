800e21efcebd2cd76b3c24b76546fdabf5f7c727
melesejr <74154647+melesejr@users.noreply.github.com>
2021-08-20 01:15:57 +0300

    add async function and fetch return data

--- app.js
+++ app.js
@@ -2,3 +2,17 @@
 const count = 10;
 const apiKey = 'bv5VfP7Ab13j7yXXS9PBC_HzLvIRZgUFhUs_VeMYikA'; 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get Photos From Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();   
+        console.log(data);
+    } catch (error) {
+
+    }
+};
+
+// On Load
+getPhotos();
