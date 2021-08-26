24a78bdc3336ec326a548c6947d621879bac4880
melesejr <74154647+melesejr@users.noreply.github.com>
2021-08-26 22:11:03 +0300

    add displayPhoto function

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const imgContainer = document.querrySelector('#img-container');
-const loader = document.querrySelector('#loader');
+const imgContainer = document.querySelector('#img-container');
+const loader = document.querySelector('#loader');
 
 let photoArray = [];
+
+// Create Elements For Links & Photos, Add To Dom
+function displayPhotos() {
+    // Run function For Each Object in PhotoArray
+    photoArray.forEach( (photo) => {
+        // Craete <a> To Link To Unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        //c Create <img> For Photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.url.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description)
+        // Put <img> Inside <a>, Then Put Both Inside imgContainer Element
+        item.appendChild(img);
+        imgContainer.appendChild(item);
+    });
+};
+
 
 // Unsplash Api
 const count = 10;
@@ -13,6 +33,7 @@ async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
         photoArray = await response.json();   
+        displayPhotos();
     } catch (error) {
 
     }
