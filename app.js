a1b284ec8d4d626ed092ef95319e7d431036a48c
melesejr <74154647+melesejr@users.noreply.github.com>
2021-08-26 23:50:16 +0300

    fix displayPhotos function code

--- app.js
+++ app.js
@@ -3,28 +3,41 @@ const loader = document.querySelector('#loader');
 
 let photoArray = [];
 
+// Helper Function To Set Attributes On Dom Element
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+};
+
 // Create Elements For Links & Photos, Add To Dom
 function displayPhotos() {
     // Run function For Each Object in PhotoArray
     for(let photo of photoArray) {
-
-    // }
-    // photoArray.forEach( (photo) => {
         // Craete <a> To Link To Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
         //c Create <img> For Photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description)
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description)
         // Put <img> Inside <a>, Then Put Both Inside imgContainer Element
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
         item.appendChild(img);
         imgContainer.appendChild(item);
     };
 };
-
 
 // Unsplash Api
 const count = 10;
